fa591957b5dcb03d299ecc2813d5467795627505
Mourud Ishmam Ahmed <ishmam1@gmail.com>
2024-02-28 21:15:13 +0600

    commenting out Mountain icon

--- components/home-page.tsx
+++ components/home-page.tsx
@@ -12,7 +12,6 @@ export function HomePage() {
     <div key="1" className="flex flex-col gap-2 min-h-[100dvh]">
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
-          <MountainIcon className="h-6 w-6" />
           <span className="sr-only">Rural School</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
@@ -233,21 +232,21 @@ export function HomePage() {
 }
 
 
-function MountainIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-    </svg>
-  )
-}
+// function MountainIcon(props) {
+//   return (
+//     <svg
+//       {...props}
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//     >
+//       <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+//     </svg>
+//   )
+// }
